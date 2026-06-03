fb52c1d10861784e9babb25eed3c44182e33f32d
Denis Chicharin <denischicharin@gmail.com>
2026-06-03 16:19:19 +0500

    fix: исправлена ошибка NameError в models.py (замена args на self)

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 class BallChain:
-    def __init__(args, balls: list[int]):
-        args.balls = balls
+    def __init__(self, balls: list[int]):
+        self.balls = balls
 
     def get_destroyed_count(self) -> int:
         total_destroyed = 0
         while True:
-            removed = args._remove_first_chain()
+            # Теперь используем self вместо args
+            removed = self._remove_first_chain()
             if removed == 0:
                 break
             total_destroyed += removed
@@ -13,18 +14,17 @@ class BallChain:
 
     def _remove_first_chain(self) -> int:
         i = 0
-        n = len(args.balls)
+        n = len(self.balls)
 
         # Проходим по списку и ищем первую группу из 3+ одинаковых элементов
         while i < n:
             count = 1
-            while i + count < n and args.balls[i + count] == args.balls[i]:
+            while i + count < n and self.balls[i + count] == self.balls[i]:
                 count += 1
 
             # Если найдена цепочка >= 3, удаляем её и возвращаем количество
-            # Сложная конструкция: удаление среза и немедленный выход для пересчета сдвигов
             if count >= 3:
-                del args.balls[i: i + count]
+                del self.balls[i: i + count]
                 return count
 
             i += count
